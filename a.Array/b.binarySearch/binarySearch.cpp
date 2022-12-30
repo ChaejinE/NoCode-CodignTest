6baba38eadab14a933c07316b0b6fe693d334f32
ChaejinE <joung6517@gmail.com>
2022-12-30 22:34:51 +0900

    주석

--- a.Array/b.binarySearch/binarySearch.cpp
+++ a.Array/b.binarySearch/binarySearch.cpp
@@ -11,19 +11,20 @@ int binarySearch(std::vector<int> nums, int target)
     int pivot;
     while(left <= right)
     {
+        // pivot 갱신
         pivot = (left + right) / 2;
-
-        if (nums[pivot] == target)
+        
+        if (nums[pivot] == target) // pivot과 target이 같으면 return
         {
             return pivot;
         }
-        else if (nums[pivot] < target)
+        else if (nums[pivot] < target) // pivot 값보다 target이 크면 찾는 값은 pivot 오른쪽에 있다는 의미
         {
-            left = pivot + 1;
+            left = pivot + 1; // left index를 pivot 오른쪽으로 갱신
         }
-        else
+        else // pivot 값보다 target이 작으면 pivot 왼쪽에 있다는 의미
         {
-            right = pivot - 1;
+            right = pivot - 1; // right index를 pivot 왼쪽으로 갱신
         } 
     }
 
