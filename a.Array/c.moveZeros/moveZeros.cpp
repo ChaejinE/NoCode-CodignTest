1bd6a948cabf422f2c196a9cd78029e7419c0482
ChaejinE <joung6517@gmail.com>
2023-01-08 00:22:37 +0900

    moveZeros

---
+++ a.Array/c.moveZeros/moveZeros.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+
+void moveZeros(std::vector<int> &nums)
+{
+    int zeroIdx = 0;
+    for (int idx = 0; idx < nums.size(); ++idx)
+    {
+        if (nums[idx] != 0)
+        {
+            std::swap(nums[zeroIdx], nums[idx]);
+            ++zeroIdx;
+        }
+    }
+}
+
+int main()
+{
+    std::vector<int> v = {0, 5, 4, 0, 3, 0, 2, 1};
+
+    moveZeros(v);
+
+    for (auto n : v)
+    {
+        std::cout << n << " ";
+    }
+    std::cout << std::endl;
+
+    return 0;
+}
